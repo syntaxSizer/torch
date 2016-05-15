50861ef95eb6ff3ed478eb705b391da6c979b618
syntaxSizer <kakuma4never@gmail.com>
2016-05-15 19:48:17 +0300

    button images , java code

--- MyTorch/app/src/main/java/androidapp/mytorch/MainActivity.java
+++ MyTorch/app/src/main/java/androidapp/mytorch/MainActivity.java
@@ -1,13 +1,56 @@
 package androidapp.mytorch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+
+import java.security.Policy;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnSwitch;
+
+    private Camera camera;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    Policy.Parameters params;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!hasFlash)
+
+        {
+            // device doesn't support flash
+            // Show alert message and close the application
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("Sorry, your device doesn't support flash light!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog,n , int which) {
+                    // closing the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
     }
+
 }
+
+
+
+
+
